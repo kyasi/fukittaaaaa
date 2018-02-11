f4bce305eed5405d6a640ce170d1a788057f9782
noriaki fukita <calonyction.aculeatum@gmail.com>
2018-02-12 00:41:29 +0900

    First step

---
+++ feature/src/main/java/com/example/fukittaaaaa/feature/CustumApplication.java
@@ -0,0 +1,25 @@
+package com.example.fukittaaaaa.feature;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterConfig;
+
+/**
+ * Created by kyasi on 2018/02/11.
+ */
+
+public class CustumApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig("CONSUMER_KEY", "CONSUMER_SECRET"))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
+    }
+}
