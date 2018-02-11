74e6024b6b68961cba3bed53718df0bb4a3df030
noriaki fukita <calonyction.aculeatum@gmail.com>
2018-02-11 23:22:34 +0900

    Create project

---
+++ feature/src/main/java/com/example/fukittaaaaa/feature/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.fukittaaaaa.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("fukittaaaaa","クリックしたよ");
+            }
+        });
+    }
+}
