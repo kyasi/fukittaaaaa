38b541aacf008e4331d35a5578c9dbdf251d8f3b
noriaki fukita <calonyction.aculeatum@gmail.com>
2018-02-25 22:24:31 +0900

    Add login button

--- feature/src/main/java/com/example/fukittaaaaa/feature/CustomApplication.java
+++ feature/src/main/java/com/example/fukittaaaaa/feature/CustomApplication.java
@@ -12,12 +12,11 @@ import com.twitter.sdk.android.core.TwitterConfig;
  * Created by kyasi on 2018/02/11.
  */
 
-public class CustumApplication extends Application {
+public class CustomApplication extends Application {
     public void onCreate() {
         super.onCreate();
         TwitterConfig config = new TwitterConfig.Builder(this)
                 .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig("CONSUMER_KEY", "CONSUMER_SECRET"))
                 .debug(true)
                 .build();
         Twitter.initialize(config);
